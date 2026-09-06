26bc31847b47efbadfa1f01e68174dbf75f3c13e
trinlol <trinlol@users.noreply.github.com>
2026-09-06 17:53:40 +0100

    chore(release): bump version to 2.6.4

--- src/common/Version.hpp
+++ src/common/Version.hpp
@@ -35,7 +35,7 @@ namespace chatterino {
  **/
 inline const QString CHATTERINO_PRODUCT_NAME =
     QStringLiteral("Chatterino Better Browser");
-inline const QString CHATTERINO_VERSION = QStringLiteral("2.6.3");
+inline const QString CHATTERINO_VERSION = QStringLiteral("2.6.4");
 
 class Version
 {
